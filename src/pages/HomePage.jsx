a83e544a7a75479b198aa1b6c57ab2c4fc707466
ETUKEMKA CHIBUIKEM <etukemkachibuikem@gmail.com>
2026-03-09 18:48:48 +0100

    use data from the backend to enerate the HTML

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,17 @@
 import axios from "axios";
-import { Header } from "../components/Header";
-import { products } from "../../starting-code/data/products.js";
+import { useEffect, useState } from "react"
+import { Header } from "../components/Header"; 
 import "./HomePage.css";
 
 export function HomePage() {
-  axios.get("http://localhost:3000/api/products")
-  .then((response) => {
-    console.log(response.data)
-  });
+  const [products, setProducts] = useState([]); 
+
+  useEffect(() => {
+    axios.get("http://localhost:3000/api/products")
+      .then((response) => {
+        setProducts(response.data)
+      });
+  }, []);
 
   return (
     <>
@@ -40,7 +44,7 @@ export function HomePage() {
                 </div>
 
                 <div className="product-price">
-                  {`$${(product.priceCents/100).toFixed(2)}`}
+                  {`$${(product.priceCents / 100).toFixed(2)}`}
                 </div>
 
                 <div className="product-quantity-container">
